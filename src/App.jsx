dc5dd28b518f07628c6571b06bd2f1be9add68a5
Daniel Tello <dantello.319@gmail.com>
2021-10-15 19:45:31 -0500

    se instalo styled-components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Form from "./components/Form";
+
+function App() {
+   return (
+      <div className="App">
+         <header>
+            <h1>To Do app</h1>
+         </header>
+      </div>
+   );
+}
+
+export default App;
